379b3ed3b75c803dbe2b7f23b664b2b7ff02e342
shingggk <97640959+shingggk@users.noreply.github.com>
2023-02-10 22:48:45 +0000

    accessing data from mongodb atlas

--- mongoTest/database.js
+++ mongoTest/database.js
@@ -3,15 +3,30 @@ mongoose.set('strictQuery', true);
 
 const MONGODB_URL = "mongodb+srv://CS628:CS628DBTEST@cluster0.uzydhqj.mongodb.net/test"
 
-exports.connect = () => {
-    mongoose.connect(MONGODB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then(
-        console.log('DB is connected!')
-    ).catch((error) => {
-        console.log('DB connection failed');
-        console.log(error);
-        process.exit(1)
-    })
-}
+mongoose.connect(MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+})
+
+// creating schema
+const profileSchema = new mongoose.Schema({
+    fullName:String,
+    class:String
+})
+
+// creating model
+const Profile = mongoose.model("profile", profileSchema);
+
+// generating data 
+const student = new Profile({
+    fullName: "TEST3",
+    class: "2021" 
+})
+
+// saving the data into a collection
+student.save()
+
+Profile.find((err, students)=> {
+    if (err){console.log(err)}
+    else {console.log(students)}
+})
